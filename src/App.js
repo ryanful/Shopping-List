5bd4989ef2f3d4fb5c5bdae913c11a49fd86570e
Raheel Y <ray37@cornell.edu>
2023-08-28 18:22:30 -0500

    Added name and amount stateto item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import Item from "./components/Item";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* Shopping List */}
       <View style={styles.content}>
         <Text style={styles.title}>Shopping List</Text>
 
@@ -15,22 +24,37 @@ export default function App() {
 
         {/* items left */}
         <View style={styles.items}>
-          <Item />
-          <Item />
+          <Item name={"Banana"} amount={1} />
+          <Item name={"Apple"} amount={2} />
         </View>
 
         {/* items found */}
       </View>
 
+      {/* Write A Task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder="Add an Item" />
+
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  addText: {},
+  addWrapper: {},
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ededed",
     // alignItems: "center",
     // justifyContent: "center",
   },
@@ -38,9 +62,20 @@ const styles = StyleSheet.create({
     paddingTop: 80,
     paddingHorizontal: 20,
   },
-  items: {},
+  input: {},
+  items: {
+    marginTop: 30,
+  },
   title: {
     fontSize: 24,
     fontWeight: "bold",
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,53 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 function Item(props) {
   return (
-    <View>
-      <Text> Item </Text>
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <Text style={styles.itemText}>
+          {props.name} - {props.amount}
+        </Text>
+      </View>
+      <View style={styles.circular}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+  item: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  square: {
+    width: 24,
+    height: 23,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+});
 
 export default Item;
